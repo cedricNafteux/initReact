230e7e2ae73560cfb940ceffd625073ec7f941d0
cedricNafteux <cedric.nafteux@gmail.com>
2022-02-17 11:58:39 +0100

    save 05

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,64 +1,8 @@
-import { Component } from 'react';
-import './App.css';
-import Product from '../Product/Product';
+import React from 'react';
+import FileNamer from '../FileNamer/FileNamer';
 
-class App extends Component {
-  render() {
-    return (
-    <Product/>
-    );
-  }
-
+function App() {
+  return <FileNamer />
 }
+
 export default App;
-
-
-
-//premier Tuto
-//  
-// import './App.css';
-// import Instructions from '../Instructions/Instructions.js';
-
-// const displayEmojiName = event => alert(event.target.id);
-// const displayAction = true;
-// const emojis = [
-//   {
-//     emoji: "😀",
-//     name: "grinning face"
-//   },
-//   {
-//     emoji: "🎉",
-//     name: "party popper"
-//   },
-//   {
-//     emoji: "💃",
-//     name: "woman dancing"
-//   }
-// ];
-
-// function App() {
-//   const greeting = "greeting";
-//   return (
-//     <div className='container'>
-//       <h1 id={greeting}>Hello world</h1>
-//       {displayAction && <p>j'ecrit en JSX</p>}      {/* si variable est true le paragraphe apparait. sinon elle est cache  */}
-//       <Instructions />
-//       <ul>
-//       {
-//           emojis.map(meuh => (
-//             <li key={meuh.name}>
-//               <button
-//                 onClick={displayEmojiName}>
-//                 <span role="img" aria-label={meuh.name} id={meuh.name}>{meuh.emoji}</span>
-//               </button>
-//             </li>
-//           ))
-//         }
-//       </ul>
-//     </div>
-//     )
-
-
-// }
-
-// export default App;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,83 +1,75 @@
-import React, { Component } from 'react';
+import React, { useReducer} from 'react';
 import './Product.css';
 
-const products = [
-    {
-      emoji: '🍦',
-      name: 'ice cream',
-      price: 5
-    },
-    {
-      emoji: '🍩',
-      name: 'donuts',
-      price: 2.5,
-    },
-    {
-      emoji: '🍉',
-      name: 'watermelon',
-      price: 4
-    }
-  ];
-  
-
- class Product extends Component {
-
-    state = {
-        cart: [],
-      }
-
-      add = (product) => {
-        this.setState(state => ({
-          cart: [...state.cart, product],
-        }))
-      }
-
-      remove = (product) => {
-        this.setState(state => {
-          const cart = [...state.cart];
-          const productIndex = cart.findIndex(p => p.name === product.name);
-          if(productIndex < 0) {
-            return;
-          }
-          cart.splice(productIndex, 1)
-          return ({
-            cart
-          })
-        })
-      }
-
-      currencyOptions = {
-          style: "currency",
-        currency: "EUR",
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      }
-    
-      getTotal = () => {
-        const total = this.state.cart.reduce((totalCost, item) => totalCost + item.price, 0);
-        return total.toLocaleString(undefined, this.currencyOptions)
-      }
-
-  render() {
-    return(
-      <div className="wrapper">
-        <div>
-          Shopping Cart: {this.state.cart.length} total items.
-        </div>
-        <div>Total {this.getTotal()}</div>
-        <div>
-          {products.map(product => (
-            <div key={product.name}>
-              <div className="product">
-                <span role="img" aria-label={product.name}>{product.emoji}</span>
-              </div>
-              <button onClick={() => this.add(product)}>Add</button>
-              <button onClick={() => this.remove(product)}>Remove</button>
-            </div>
-          ))}
-        </div>
-      </div>
-    )
-  }
+const currencyOptions = {
+  style: "currency",
+  currency: "EUR",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
 }
-export default Product;
+
+function getTotal(totalCart) {
+  return totalCart.toLocaleString(undefined, currencyOptions)
+}
+const products = [
+  {
+    emoji: '🍦',
+    name: 'ice cream',
+    price: 5
+  },
+  {
+    emoji: '🍩',
+    name: 'donuts',
+    price: 2.5,
+  },
+  {
+    emoji: '🍉',
+    name: 'watermelon',
+    price: 4
+  }
+];
+
+function cartReducer(state, product) {
+  return [...state, product]
+}
+
+function totalReducer(state, price) {
+  return state + price;
+}
+
+export default function Product()
+{
+  const [cart, setCart] = useReducer(cartReducer, []);
+  const [total, setTotal] = useReducer(totalReducer, 0);
+
+  function add(product) {
+    setCart(product.name);
+    setTotal(product.price);
+  }
+  
+  return(
+    <div className="wrapper">
+      <div>
+        Panier: {cart.length} articles.
+      </div>
+      <div>Total: {getTotal(total)}</div>
+      <div>
+        {products.map(product => (
+          <div key={product.name}>
+            <div className="product">
+              <span role="img" aria-label={product.name}>{product.emoji}</span>
+            </div>
+            <button onClick={() => add(product)}>Ajouter</button>
+            <button
+              onClick={() => {
+                setCart([]);
+                setTotal(0);
+              }
+            }
+              >Enlever</button>
+          </div>
+        ))}
+        </div>
+    </div>
+  )
+}
